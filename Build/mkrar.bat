2e44814d709953863cbddc70eac42966d49ef700
lasersquad0 <lasersquad@gmail.com>
2025-07-28 17:48:13 +0300

    Reflects project structure changes

--- Build/mkrar.bat
+++ Build/mkrar.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\WinRAR\rar.exe" a -ep FindexX.1.1.0.zip ..\Win64\Release\FinderX.exe ..\README.txt ..\changelog.txt
+"c:\Program Files\WinRAR\rar.exe" a -ep FinderX.1.2.0.zip ..\prj\FinderX\Win64\Release\FinderX.exe MFTReaderDLL.dll ..\TODO.txt ..\README.md ..\changelog.txt
