d19f17988c75cde47a782cbaf050075b0448c923
lasersquad0 <lasersquad@gmail.com>
2026-06-23 15:46:04 +0300

    now it packs to real zip format

--- Build/packbin.bat
+++ Build/packbin.bat
@@ -1,1 +1,3 @@
-"c:\Program Files\WinRAR\rar.exe" a -ep FinderX.1.3.0.zip ..\prj\FinderX\Win64\Release\FinderX.exe MFTReaderDLL_64.dll ..\TODO.txt ..\README.md ..\changelog.txt
+rem "c:\Program Files\WinRAR\rar.exe" a -ep FinderX.1.3.0.zip ..\prj\FinderX\Win64\Release\FinderX.exe MFTReaderDLL_64.dll ..\TODO.txt ..\README.md ..\changelog.txt
+
+"c:\Program Files\WinRAR\winrar.exe" a -afzip -ep FinderX.1.3.1.zip ..\prj\FinderX\Win64\Release\FinderX.exe FinderX.ru MFTReaderDLL_64.dll ..\TODO.txt ..\README.md ..\changelog.txt
